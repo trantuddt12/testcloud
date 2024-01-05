f219decb744b06eea65875ddbec3263da7f80028
tuv.tran <tuv.tran@tx-tech.com>
2024-01-05 15:38:32 +0700

    add 2 service

---
+++ testcloud/TestApi/src/main/java/com/tutv11/testapi/TestApiApplication.java
@@ -0,0 +1,14 @@
+package com.tutv11.testapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestApiApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SpringApplication.run(TestApiApplication.class, args);
+	}
+
+}
---
+++ testcloud/testconfig/src/main/java/com/tutv11/testconfig/TestConfigApplication.java
@@ -0,0 +1,17 @@
+package com.tutv11.testconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@SpringBootApplication
+@EnableConfigServer
+public class TestConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestConfigApplication.class, args);
+
+	}
+
+}
