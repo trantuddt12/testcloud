5a453e3ff43ead6e63b85d617dab7d528bebd087
tuv.tran <tuv.tran@tx-tech.com>
2024-01-05 13:19:58 +0700

    first commit

---
+++ testcloud/src/main/java/com/tutv11/testcloud/TestcloudApplication.java
@@ -0,0 +1,13 @@
+package com.tutv11.testcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcloudApplication.class, args);
+	}
+
+}
---
+++ testcloud/src/test/java/com/tutv11/testcloud/TestcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutv11.testcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
